856f56bcc9db84a023162725d622042a703aca77
Aaron Thapa <notsmart98@gmail.com>
2024-12-03 00:30:49 -0500

    day 2 party

---
+++ day3/part2/src/main.rs
@@ -0,0 +1,61 @@
+fn main() {
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    println!("{}", input);
+    let mul_indices: Vec<usize> = input.match_indices("mul(").map(|x| x.0).collect();
+    println!("{:?}", mul_indices);
+
+    let mut do_indices: Vec<usize> = input.match_indices("do()").map(|x| x.0).collect();
+    do_indices.insert(0, 0);
+    println!("{:?}", do_indices);
+
+    let dont_indices: Vec<usize> = input.match_indices("don't()").map(|x| x.0).collect();
+    println!("{:?}", dont_indices);
+
+    let mut sum = 0;
+
+    for mul_index in mul_indices {
+        if !do_do(mul_index, do_indices.clone(), dont_indices.clone()) {
+            continue;
+        }
+        let start_index = mul_index + 4;
+        // find comma
+        let Some(comma_index) = input[start_index..].find(",") else {
+            continue;
+        };
+        if comma_index > 3 || !input[start_index .. start_index + comma_index].chars().all(|x| x.is_digit(10)) {
+            continue;
+        }
+        // find close paren
+        let Some(close_paren_index) = input[start_index + comma_index + 1..].find(")") else {
+            continue;
+        };
+        if close_paren_index > 3 || !input[start_index + comma_index + 1.. start_index + comma_index + 1 + close_paren_index].chars().all(|x| x.is_digit(10)) {
+            continue;
+        }
+        let first_num: i32 = input[start_index..start_index + comma_index].parse().unwrap();
+        let second_num: i32 = input[start_index + comma_index + 1..start_index + comma_index + 1 + close_paren_index].parse().unwrap();
+
+        sum += first_num * second_num;
+
+        println!("{} {} {} | {} {}", start_index, comma_index, close_paren_index, first_num, second_num);
+    }
+    println!("{}", sum);
+}
+
+fn do_do(mul_index: usize, do_indices: Vec<usize>, dont_indices: Vec<usize>) -> bool {
+    let mut greatest_do_index: usize = 0;
+    for do_index in do_indices {
+        if do_index > mul_index {
+            break;
+        }
+        greatest_do_index = do_index;
+    }
+    let mut greatest_dont_index: usize = 0;
+    for dont_index in dont_indices {
+        if dont_index > mul_index {
+            break;
+        }
+        greatest_dont_index = dont_index;
+    }
+    greatest_do_index >= greatest_dont_index
+}
