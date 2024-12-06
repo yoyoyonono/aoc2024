74dbc0bf5f5e3b485fa89181b46ab777d7d4b71d
Aaron Thapa <notsmart98@gmail.com>
2024-12-06 01:04:32 -0500

    optimize

--- day6/part2/src/main.rs
+++ day6/part2/src/main.rs
@@ -30,10 +30,15 @@ fn main() {
         })
         .collect();
 
+    let affected_spaces = find_spaces(grid.clone());
+
     let sum: i32 = (0..grid.len())
         .map(|row| {
             (0..grid[row].len())
                 .map(|col| {
+                    if !affected_spaces.contains(&[row, col]) {
+                        return 0;
+                    }
                     let mut change = grid.clone();
                     if !(change[row][col] == None || change[row][col] == Some(GridSpace::Guard)) {
                         return 0;
@@ -48,6 +53,78 @@ fn main() {
         })
         .sum();
     println!("{sum}");
+}
+
+fn find_spaces(grid: Vec<Vec<Option<GridSpace>>>) -> HashSet<[usize; 2]> {
+
+    // find guard position
+    let mut guard_pos: [isize; 2] = [0, 0];
+
+    for (row_index, row) in grid.iter().enumerate() {
+        for (col_index, space) in row.iter().enumerate() {
+            match space {
+                Some(GridSpace::Guard) => {
+                    guard_pos = [row_index.try_into().unwrap(), col_index.try_into().unwrap()];
+                }
+                _ => {}
+            }
+        }
+    }
+
+    let mut current_direction = Direction::Up;
+    let mut visited_spaces = HashSet::new();
+
+    while (guard_pos[0] >= 0)
+        && (guard_pos[0] < grid.len().try_into().unwrap())
+        && (guard_pos[1] >= 0)
+        && (guard_pos[1] < grid[0].len().try_into().unwrap())
+    {
+        let guard_row: usize = guard_pos[0].try_into().unwrap();
+        let guard_col: usize = guard_pos[1].try_into().unwrap();
+
+        // check
+        match grid[guard_row][guard_col] {
+            Some(GridSpace::Obstruction) => match current_direction {
+                Direction::Up => {
+                    guard_pos[0] += 1;
+                    current_direction = Direction::Right;
+                }
+                Direction::Down => {
+                    guard_pos[0] -= 1;
+                    current_direction = Direction::Left;
+                }
+                Direction::Left => {
+                    guard_pos[1] += 1;
+                    current_direction = Direction::Up;
+                }
+                Direction::Right => {
+                    guard_pos[1] -= 1;
+                    current_direction = Direction::Down;
+                }
+            },
+            _ => {}
+        }
+
+        visited_spaces.insert([guard_pos[0].try_into().unwrap(), guard_pos[1].try_into().unwrap()]);
+
+        // move
+        match current_direction {
+            Direction::Up => {
+                guard_pos[0] -= 1;
+            }
+            Direction::Down => {
+                guard_pos[0] += 1;
+            }
+            Direction::Left => {
+                guard_pos[1] -= 1;
+            }
+            Direction::Right => {
+                guard_pos[1] += 1;
+            }
+        };
+    }
+
+    visited_spaces
 }
 
 fn does_loop(grid: Vec<Vec<Option<GridSpace>>>) -> bool {
