30f4782e29b3aab8123459e8b21065b551eaf159
Aaron Thapa <notsmart98@gmail.com>
2024-12-06 19:26:06 -0500

    optimize

--- day6/part2/src/main.rs
+++ day6/part2/src/main.rs
@@ -133,33 +133,24 @@ fn find_spaces(grid: Vec<Vec<Option<GridSpace>>>) -> HashSet<[usize; 2]> {
 
 fn does_loop(grid: Vec<Vec<Option<GridSpace>>>) -> bool {
     // find guard position
-    let mut guard_pos: [isize; 2] = [0, 0];
-
-    for (row_index, row) in grid.iter().enumerate() {
+    'outer: for (row_index, row) in grid.iter().enumerate() {
         for (col_index, space) in row.iter().enumerate() {
-            match space {
-                Some(GridSpace::Guard) => {
-                    guard_pos = [row_index.try_into().unwrap(), col_index.try_into().unwrap()];
-                }
-                _ => {}
+            if let Some(GridSpace::Guard) = space {
+                guard_pos = [row_index as isize, col_index as isize];
+                break 'outer;
             }
         }
     }
-
     let mut current_direction = Direction::Up;
     let mut visited_spaces = HashSet::with_capacity(grid.len() * grid[0].len() * 4);
-
-    while (guard_pos[0] >= 0)
-        && (guard_pos[0] < grid.len().try_into().unwrap())
-        && (guard_pos[1] >= 0)
-        && (guard_pos[1] < grid[0].len().try_into().unwrap())
-    {
-        let guard_row: usize = guard_pos[0].try_into().unwrap();
-        let guard_col: usize = guard_pos[1].try_into().unwrap();
-
+    let rows = grid.len() as isize;
+    let cols = grid[0].len() as isize;
+    while guard_pos[0] >= 0 && guard_pos[0] < rows && guard_pos[1] >= 0 && guard_pos[1] < cols {
+        let guard_row = guard_pos[0] as usize;
+        let guard_col = guard_pos[1] as usize;
+        if let Some(GridSpace::Obstruction) = grid[guard_row][guard_col] {
+            match current_direction {
         // check
-        match grid[guard_row][guard_col] {
-            Some(GridSpace::Obstruction) => match current_direction {
                 Direction::Up => {
                     guard_pos[0] += 1;
                     current_direction = Direction::Right;
@@ -176,28 +167,19 @@ fn does_loop(grid: Vec<Vec<Option<GridSpace>>>) -> bool {
                     guard_pos[1] -= 1;
                     current_direction = Direction::Down;
                 }
-            },
-            _ => {}
+            }
         }
-        let entry = (guard_pos[0], guard_pos[1], current_direction.clone());
+        let entry = (guard_pos[0], guard_pos[1], current_direction as u8);
         if !visited_spaces.insert(entry) {
             return true;
         }
         // move
         match current_direction {
-            Direction::Up => {
-                guard_pos[0] -= 1;
-            }
-            Direction::Down => {
-                guard_pos[0] += 1;
-            }
-            Direction::Left => {
-                guard_pos[1] -= 1;
-            }
-            Direction::Right => {
-                guard_pos[1] += 1;
-            }
-        };
+            Direction::Up => guard_pos[0] -= 1,
+            Direction::Down => guard_pos[0] += 1,
+            Direction::Left => guard_pos[1] -= 1,
+            Direction::Right => guard_pos[1] += 1,
+        }
     }
     false
 }
